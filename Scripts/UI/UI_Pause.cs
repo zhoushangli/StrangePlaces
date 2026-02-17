4e2f24f9f35ad2424dbbe9d4a7e29acbd70c6c02
superz63 <wangzhh1@shanghaitech.edu.cn>
2026-02-17 21:55:28 +0800

    [WangZhihang]试做了游戏场景 暂停UI，开始界面UI正在制作

--- Scripts/UI/UI_Pause.cs
+++ Scripts/UI/UI_Pause.cs
@@ -9,18 +9,23 @@ namespace Protogame2D.UI;
 /// </summary>
 public partial class UI_Pause : UIBase
 {
-    private Button _resumeButton;
-    private Button _backButton;
+    [Export]
+    private TextureButton _resumeButton;
+    [Export]
+    private TextureButton _backButton;
+    [Export]
+    private TextureButton _quitButton;
 
     public override void _Ready()
     {
-        _resumeButton = GetNode<Button>("CenterContainer/VBoxContainer/ResumeButton");
-        _backButton = GetNode<Button>("CenterContainer/VBoxContainer/BackToMenuButton");
+        // _resumeButton = GetNode<Button>("CenterContainer/VBoxContainer/ResumeButton");
+        // _backButton = GetNode<Button>("CenterContainer/VBoxContainer/BackToMenuButton");
 
         _resumeButton.Pressed += OnResumePressed;
         _backButton.Pressed += OnBackPressed;
+        _quitButton.Pressed += OnQuitPressed;
     }
-
+    
     private void OnResumePressed()
     {
         QueueFree();
@@ -30,4 +35,26 @@ public partial class UI_Pause : UIBase
     {
         Game.Instance.Get<GameStateService>().ChangeGameState(GameState.MainMenu);
     }
+    private void OnQuitPressed()
+    {
+        //关闭游戏
+        GetTree().Quit();
+    }
+
+    public override void OnOpen(object args)
+    {
+        //我把该函数功能理解为_Ready函数。_xxxButton在inspector中赋值
+        // _resumeButton = GetNode<Button>("NinePatchRect/VBoxContainer/ResumeButton2");
+        // _backButton = GetNode<Button>("NinePatchRect/VBoxContainer/BackToMenuButton2");
+
+        _resumeButton.Pressed += OnResumePressed;
+        _backButton.Pressed += OnBackPressed;
+        _quitButton.Pressed += OnQuitPressed;
+    }
+
+    public override void OnClose()
+    {
+        //TODO
+    }
+
 }
